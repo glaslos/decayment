29b930784ed6917bad87160f2fe31d1b99c8b7e1
glaslos <lukas.rist@bluecoat.com>
2016-08-25 14:35:25 +0200

    doh

--- decayment.go
+++ decayment.go
@@ -1,6 +1,6 @@
 /*
 Copyright 2016 Lukas Rist. All rights reserved.
-Use of this source code is governed by a MIT-style
+Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file.
 */
 
